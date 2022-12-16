2703e3430e7c1c927da6000a0a2f8ed8e9c131e6
DESKTOP-145HVSV\Shahbaaz Khan <shahbaaz.khan@alphanetworks.global>
2022-12-16 12:22:28 +0530

    At Math Surfaces Leaving the Grid

--- graph-vis/Assets/Scripts/FunctionLibrary.cs
+++ graph-vis/Assets/Scripts/FunctionLibrary.cs
@@ -5,24 +5,34 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public static float Wave(float x, float t)
-    {
-        return Sin(PI * (x + t));
-    }
-    
-    public static float MultiWave(float x, float t)
-    {
-        float y = Sin(PI * (x + t));
-        y += Sin(2f* PI * (x + t)) * (1f / 2f);
-     
-        // divide result by 1.5 to limit the range of function within -1 and 1
-        // or else limits were gonna become -1.5 to 1.5
-        // (sin value can be 1 or -1 so sin plus sin2pi/2 will be 1 + 1/2 which will be 1.5)
-        // dividing a variable takes more time than multiplying for a compiler
-        // instead of 1.5 we write 2/3 
-        //we always prefer multiplication to division for efficiency 
-        return y * (2f / 3f);
+    public delegate float Function(float x, float z, float t);
+    public enum FunctionName { Wave, MultiWave, Ripple }
 
-        // we are at Division requires a bit more work than multiplication
+    static Function[] functions = {Wave,MultiWave,Ripple};
+    public static Function GetFunction(FunctionName name)
+    {
+        return functions[(int)name];
+    } 
+
+    public static float Wave(float x, float z, float t)
+    {
+        return Sin(PI * (x +z+ t));
+    }
+
+    public static float MultiWave(float x, float z, float t)
+    {
+        float y = Sin(PI * (x + 0.5f * t));
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f);
+
+
+
+    }
+
+    public static float Ripple(float x, float z, float t)
+    {
+        float d = Sqrt(x * x + z * z);
+        float y = Sin(PI * (4f* d - t));
+        return y / (1f+10f * d);
     }
 }
--- graph-vis/Assets/Scripts/Graph.cs
+++ graph-vis/Assets/Scripts/Graph.cs
@@ -7,23 +7,30 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] Transform pointPrefab;
     [SerializeField,Range(10,100)] int resolution = 10;
+    [SerializeField]  FunctionLibrary.FunctionName function;
 
     Transform[] points;
 
     void Awake()
     {
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, x = 0,z = 0; i < points.Length; i++,x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPrefab);
-
+            
             //setting cubes from -1 to 1
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
 
             point.localPosition = position;   
             point.localScale = scale;
@@ -33,13 +40,14 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        FunctionLibrary.Function f  = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = FunctionLibrary.Wave(position.x, time);
-
+                
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
